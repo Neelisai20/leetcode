09fd5229185040c60481f97e6f4fdcfbb82e22c9
Neelisai20 <116020583+Neelisai20@users.noreply.github.com>
2024-12-22 00:30:34 +0530

    Time: 4 ms (42.39%) | Memory: 84.2 MB (13.26%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -9,26 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+ class Solution {
 public:
-     TreeNode* reverseOddLevels(TreeNode* root) {
-        traverseDFS(root->left, root->right, 0);
-        return root;
-    }
-
-private:
-    void traverseDFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
-        if (leftChild == nullptr || rightChild == nullptr) {
-            return;
-        }
-        
-        if (level % 2 == 0) {
-            int temp = leftChild->val;
-            leftChild->val = rightChild->val;
-            rightChild->val = temp;
+   TreeNode* reverseOddLevels(TreeNode* root) {
+        if (!root) {
+            return nullptr; 
         }
 
-        traverseDFS(leftChild->left, rightChild->right, level + 1);
-        traverseDFS(leftChild->right, rightChild->left, level + 1);
+        queue<TreeNode*> q;
+        q.push(root); 
+        int level = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<TreeNode*> prev;
+
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                prev.push_back(node);
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            
+            if (level % 2 == 1) {
+                int left = 0, right = prev.size() - 1;
+                while (left < right) {
+                    int temp = prev[left]->val;
+                    prev[left]->val =
+                        prev[right]->val;
+                    prev[right]->val = temp;
+                    left++;
+                    right--;
+                }
+            }
+
+            level++;
+        }
+
+        return root;  
     }
 };
